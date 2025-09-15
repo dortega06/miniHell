a5d3d55c2217fa20fd4ddbf6dbeaf06b54f279cf
Diana Ortega Suarez <dortega-@cbr12s4.42barcelona.com>
2025-09-15 20:22:17 +0200

    include

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,16 @@
 /*   By: diespino <diespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 13:01:04 by diespino          #+#    #+#             */
-/*   Updated: 2025/09/15 16:11:53 by dortega-         ###   ########.fr       */
+/*   Updated: 2025/09/15 20:22:04 by dortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*═════════════════════════════ [  LIBS  ] ═══════════════════════════════════*/
+# include "../libft/libft.h"
+//global libs
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
@@ -27,27 +30,59 @@
 # include <signal.h>
 # include <termios.h>
 # include <termcap.h>
-# include "../libft/libft.h"
 
-typedef enum token_type
+/*═══════════════════════════ [  ENUMS  ] ════════════════════════════════════*/
+
+typedef enum e_token
 {
-	WORD,//		cmd, args, etc
-	PIPE,//		|
-	REDIR_IN,//	<
-	REDIR_OUT,//	>
-	APPEND,// 	>>
-	HEREDOC,// 	<<
-	ERROR,
-} 	t_token_type;
+	T_GENERAL,
+	T_CMD,       // comando
+	T_PIPE,      // | pipe
+	T_REDIR_IN,  // < REDIR_IN
+	T_INFILE,    // infile
+	T_HEREDOC,   // << HEREDOC
+	T_LIMITER,   // (str) para terminar de introducir datos
+	T_REDIR_OUT, // > REDIR_OUT
+	T_OUTFILE,   // outfile
+	T_APPEND,    // >> APPEND
+	T_SIZE
+}			t_token;
 
+/*══════════════════════════ [  STRUCTS  ] ═══════════════════════════════════*/
+typedef struct s_lexer
+{
+	int				index; //  indice del token
+	char			*data; //  contenido (valor literal)
+	int				type;  //  numero equivalente al token (enum)
+	struct s_lexer	*next; //  siguiente elemento en la lista
+}	t_lexer;
+/*
 typedef struct s_token
 {
 	t_token_type	type;// WORD, PIPE,...
 	char			*value;// palabra o simbolo
 	int				position;// posicion palabra
 	struct s_token	*next;
-}			t_token;
+}			t_token;*/
+typedef struct s_parser
+{
+	char			*cmd; //comando que será ejecutado
+	int 			redir_in; //entrada
+	int 			redir_out; //salida
+	struct s_parser	*next; //siguiente elemento en la lista
+}	t_parser;
 
+typedef struct s_shell
+{
+	char		**paths; //variables de entorno del sistema
+	char		**cmd_args; //comando seguido de argumentos
+	int			count_cmd_args; //cantidad de comando + argumentos
+	t_env		*env; //lista de nodos que representa `envp`
+	t_lexer		*lexer; //lista de nodos que separa los tokens
+	t_parser	*parser; //lista de nodos que separa los comandos
+	int			exit_status; //entero que representa el estado de salida
+}	t_shell;
+/*
 typedef struct s_parser {
 	char		*cmd;
 	char		*path;
@@ -67,11 +102,15 @@ typedef struct s_cmd {
     t_token		*tokens;        // Lista de palabras/argumentos
     t_redir		*redirects;     // Lista de redirecciones
     struct s_cmd	*next;         // Siguiente comando (para pipes)
-}   t_cmd;
-// TOKEN UTILS
+}   t_cmd;*/
+
+/*═════════════════════════ [  FUNCTIONS  ] ══════════════════════════════════*/
+/*---------------------------- [  lexer  ] -----------------------------------*/
 int		is_quotes(char c);
 t_token_type	is_symbol(char *str);
 int validate_syntax(t_cmd *commands);
 int print_error(const char *prefix, const char *msg, int code);
+
+/*--------------------------- [  parser  ] -----------------------------------*/
 
 #endif
