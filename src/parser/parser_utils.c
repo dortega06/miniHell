fc686e688399046deb2f024431d0120df02c70eb
Diana Ortega Suarez <dortega-@cbr14s6.42barcelona.com>
2025-09-20 17:11:56 +0200

    parser

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,12 +6,13 @@
 /*   By: dortega- <dortega-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 20:56:40 by dortega-          #+#    #+#             */
-/*   Updated: 2025/09/15 21:11:34 by dortega-         ###   ########.fr       */
+/*   Updated: 2025/09/20 17:11:25 by dortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*Asigna índices a cada nodo en una lista enlazada de tokens.*/
 void	ft_index(t_lexer *lex)
 {
 	t_lexer	*tmp;
@@ -25,4 +26,20 @@ void	ft_index(t_lexer *lex)
 		tmp = tmp->next;
 		i++;
 	}
-} 
+}
+
+int	ft_count_pipes(t_lexer *lex)
+{
+	t_lexer	*tmp;
+	int		i;
+
+	tmp = lex;
+	i = 0;
+	while (tmp)
+	{
+		if (tmp->type == T_PIPE)
+			i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
