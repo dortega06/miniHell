4269262e19b573fb4dc091a9f9be43dbd0301c79
Diana Ortega Suarez <dortega-@cbr14s6.42barcelona.com>
2025-10-15 18:55:03 +0200

    parseo

---
+++ src/parser/fill_node.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill_node.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dortega- <dortega-@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:15:25 by dortega-          #+#    #+#             */
+/*   Updated: 2025/10/15 18:45:04 by dortega-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_fill_node(t_lexer *lex, t_parser **cmd_node, int start, int end)
+{
+	t_lexer	*tmp;
+
+	tmp = lex;
+	(*cmd_node)->redir_in = STDIN_FILENO;
+	(*cmd_node)->redir_out = STDOUT_FILENO;
+}
+
+void	ft_redirect(t_lexer *tmp, t_parser **cmd_node)
+{
+	int	fd;
+
+	if (tmp->type == T_REDIR_IN)
+	{
+		fd = open(tmp->next->data, O_RDONLY);
+		(*cmd_node)->redir_in = fd;
+	}
+	else if (tmp->type == T_REDIR_OUT)
+	{
+		fd = open(tmp->next->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		(*cmd_node)->redir_out = fd;
+	}
+	else if (tmp->type == T_APPEND)
+	{
+		fd = open(tmp->next->data, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		(*cmd_node)->redir_out = fd;
+	}
+	else if (tmp->type == T_HEREDOC)
+	{
+		fd = ft_heredoc(tmp->next->data);
+		(*cmd_node)->redir_in = fd;
+	}
+}
+
+void	fill_cmd(t_lexer *tmp, t_parser **cmd_node)
+{
+	int	len;
+
+	len = ft_len_cmd(tmp);
+	(*cmd_node)->cmd = ft_calloc(len, sizeof(char));
+	while (tmp && tmp->type == T_CMD)
+	{
+		ft_strlcat((*cmd_node)->cmd, tmp->data, len);
+		if (tmp->next && tmp->next->type == T_CMD)
+			ft_strlcat((*cmd_node)->cmd, " ", len);
+		tmp = tmp->next;
+	}
+}
+
+int	ft_len_cmd(t_lexer *tmp)
+{
+	int	len;
+
+	len = 0;
+	while (tmp && tmp->type == T_CMD)
+	{
+		len += (ft_strlen(tmp->data) + 1);
+		tmp = tmp->next;
+	}
+	return (len);
+}
---
+++ src/parser/fill_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dortega- <dortega-@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:52:59 by dortega-          #+#    #+#             */
+/*   Updated: 2025/10/15 18:53:59 by dortega-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+t_lexer	*find_start_node(t_lexer *lex, int start)
+{
+	t_lexer	*tmp;
+
+	tmp = lex;
+	while (tmp && tmp->index != start)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+void	process_redirections(t_lexer *start_node, t_parser **cmd_node, int *start, int end)
+{
+	t_lexer	*tmp;
+	int		aux;
+
+	tmp = start_node;
+	aux = *start;
+	while(tmp && aux <= end)
+	{
+		if (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT
+			|| tmp->type == T_APPEND || tmp->type == T_HEREDOC)
+		{
+			if (tmp->index == *start)
+				*start += 2;
+			ft_redirect(tmp, cmd_node);
+		}
+		tmp = tmp->next;
+		aux++;
+	}
+}
+
+void	fill_redir(t_lexer *lex, t_parser **cmd_node, int *start, int end)
+{
+	t_lexer	*start_node;
+
+	start_node = find_start_node(lex, *start);
+	process_redirections(start_node, cmd_node, start, end);
