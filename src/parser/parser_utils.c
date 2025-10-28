56c78a5a7354b3e32ca330dd713c2296cb0cb5c5
Diana Ortega Suarez <dortega-@car11s3.42barcelona.com>
2025-10-28 17:24:59 +0100

    push

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,42 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fill_utils.c                                       :+:      :+:    :+:   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dortega- <dortega-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/15 18:52:59 by dortega-          #+#    #+#             */
-/*   Updated: 2025/10/28 12:41:09 by dortega-         ###   ########.fr       */
+/*   Created: 2025/10/28 13:25:08 by dortega-          #+#    #+#             */
+/*   Updated: 2025/10/28 15:40:11 by dortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	fill_redir(t_lexer *lex, t_parser **cmd_node, int *start, int end)
+void	ft_index(t_lexer *lex)
 {
 	t_lexer	*tmp;
-	int	aux;
+	int		i;
 
 	tmp = lex;
-	aux = *start;
-	while(tmp && aux <= end)
+	i = 0;
+	while (tmp)
 	{
-		if (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT
-			|| tmp->type == T_APPEND || tmp->type == T_HEREDOC)
-		{
-			if (tmp->index == *start)
-				*start += 2;
-			ft_redirect(tmp, cmd_node);
-		}
+		tmp->index = i;
 		tmp = tmp->next;
-		aux++;
+		i++;
 	}
 }
 
-void ft_memfree(void *ptr)
+int	ft_count_pipes(t_lexer *lex)
 {
-	if (!ptr)
-		return ;
-	free(ptr);
-	ptr = NULL;
+	t_lexer	*tmp;
+	int		i;
+
+	tmp = lex;
+	i = 0;
+	while (tmp)
+	{
+		if (tmp->type == T_PIPE)
+			i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
+
+int	get_last(t_lexer *lex, int start)
+{
+	t_lexer	*tmp;
+	int		last;
+
+	last = -1;
+	tmp = lex;
+	while (tmp && tmp->next)
+	{
+		if (tmp->type == T_PIPE && tmp->index > start)
+			return (tmp->index - 1);
+		tmp = tmp->next;
+	}
+	last = tmp->index;
+	return (last);
 }
