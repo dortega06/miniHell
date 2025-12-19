3bc2330a1895c306ee9af445012e5b2dafd81d67
Diego Rene Espinoza Fernandez <diespino@cbr12s6.42barcelona.com>
2025-12-19 19:51:18 +0100

    unset hecho (falta testear)

--- src/built_ins/ft_unset.c
+++ src/built_ins/ft_unset.c
@@ -6,15 +6,66 @@
 /*   By: diespino <diespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:52:52 by diespino          #+#    #+#             */
-/*   Updated: 2025/12/16 12:05:46 by diespino         ###   ########.fr       */
+/*   Updated: 2025/12/19 19:49:15 by diespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	del_node(t_env *node)
+{
+	free(node->var_name);
+	free(node->var_value);
+	free(node);
+}
+
+void	cmp_var(char *var, t_shell *msh)
+{
+	t_env	*env;
+	t_env	*tmp;
+	t_env	*next;
+
+	env = msh->env;
+	next = NULL;
+	while (env && env->next)
+	{
+		if (!ft_strcmp(env->next->var_name, var))
+		{
+			tmp = env;
+			env = env->next;
+			if (env->next != NULL)
+				next = env->next;
+			del_node(env);
+			tmp->next = next;
+			break ;
+
+		}
+		env = env->next;
+	}
+}
+
 void	ft_unset(t_shell *msh)
 {
-	msh->exit_status = 0;
+	int	i;
+	t_env	*tmp;
+
 	if (msh->count_cmd_args == 1)
+	{
+		msh->exit_status = 1;
 		return ;
+	}
+	i = 1;
+	while (msh->cmd_args[i])
+	{
+		if (!ft_strcmp(msh->env->var_name, msh->cmd_args[i]))
+		{
+			tmp = msh->env;
+			msh->env = msh->env->next;
+			del_node(tmp);
+		}
+		else
+			cmp_var(msh->cmd_args[i], msh);
+		i++;
+	}
+	msh->exit_status = 0;
 }
